bca820c94e3c15125a2209dd5d66e27080cb00ba
deathmond1987 <deathmond1987@gmail.com>
2023-06-01 11:32:17 +0300

    Update zsh_home_install.sh

--- zsh_home_install.sh
+++ zsh_home_install.sh
@@ -1,48 +1,103 @@
 #!/usr/bin/env bash
 set -xe
 
-dnf install git zsh -y
-    if [ -z "$PROXY" ]; then
+install_git_zsh () {
+    if command -v dnf >& ; then
+        PACKAGE_MANAGER="dnf install git zsh -y"
+    elif command -v apt-get >& ; then
+        PACKAGE_MANAGER="apt install git zsh -y"
+    elif command -v pacman >&; then
+        PACKAGE_MANAGER="pacman -S --noconfirm git zsh"
+    elif command -v zypper>&; then
+        PACKAGE_MANAGER="zypper install -y git zsh"
+    fi
+}
+
+config_proxy () {
+    if [ -z "$HTTP_PROXY:-" ]; then
         #get oh-my-zsh
-        sh -c "$(curl -fsSL -x http://$PROXY https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        sh -c "$(curl -fsSL -x $PROXY https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         #config git with proxy
         git config --global http.proxy http://"$PROXY"
         git config --global http.proxyAuthMethod 'basic' 
         git config --global http.sslVerify false
     fi
-#get oh-my zsh without proxy
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-#get zsh syntax highlightning plugin
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-#get zsh autosuggections plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-#enabling plugins in .zshrc config file
-sed -i 's/plugins=(git)/plugins=(docker docker-compose systemd git zsh-autosuggestions zsh-syntax-highlighting sudo)/g' $HOME/.zshrc
-#enabling stacking options for docker suggections. need to docker -it working with autosuggections
-echo -e "zstyle ':completion:*:*:docker:*' option-stacking yes\nzstyle ':completion:*:*:docker-*:*' option-stacking yes" >> .zshrc
-#get powerlevel10k theme for zsh
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-#enable powerlevel10k theme in zsh config
-sed -i 's:ZSH_THEME="robbyrussell":ZSH_THEME="powerlevel10k/powerlevel10k":g' $HOME/.zshrc
-#this need do manually, so asking for that
-echo -e "You must enable fonts in your terminal\nSee here: https://github.com/romkatv/powerlevel10k"
-#changing default shell
-chsh -s /bin/zsh
-#links to new programs
-echo "You need manually install:
-https://github.com/sharkdp/bat/releases
-https://github.com/lsd-rs/lsd/releases
-https://github.com/bootandy/dust/releases
-https://github.com/muesli/duf/releases
-https://github.com/aristocratos/btop/releases
-https://github.com/dundee/gdu"
-  
-#create aliases to links new programs to defaults
-echo -e 'alias yay="sudo dnf update"
-alias htop="btop"
-alias du="dust"
-alias df="duf"
-alias cat="bat -pp -P"
-alias nano="micro"
-alias ls="lsd"
-alias ncdu="gdu"' >> .zshrc    
+}
+
+install_oh_my_zsh () {
+    #get oh-my zsh without proxy
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+install_plugins () {
+    #get zsh syntax highlightning plugin
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    #get zsh autosuggections plugin
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    #enabling plugins in .zshrc config file
+    sed -i 's/plugins=(git)/plugins=(docker docker-compose systemd git zsh-autosuggestions zsh-syntax-highlighting sudo)/g' $HOME/.zshrc
+}
+
+install_powerlevel () {
+    #get powerlevel10k theme for zsh
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+    #enable powerlevel10k theme in zsh config
+    sed -i 's:ZSH_THEME="robbyrussell":ZSH_THEME="powerlevel10k/powerlevel10k":g' $HOME/.zshrc
+}
+
+fix_zsh_docker () {
+    #enabling stacking options for docker suggections. need to docker -it working with autosuggections
+    echo -e "zstyle ':completion:*:*:docker:*' option-stacking yes\nzstyle ':completion:*:*:docker-*:*' option-stacking yes" >> .zshrc
+}
+
+config_font() {
+    #this need do manually, so asking for that
+    echo -e "You must enable fonts in your terminal\nSee here: https://github.com/romkatv/powerlevel10k"
+}
+
+change_shell () {
+    #changing default shell
+    chsh -s /bin/zsh
+}
+
+linux_2023 () {
+    #links to new programs
+    echo "You need manually install:
+    https://github.com/sharkdp/bat/releases
+    https://github.com/lsd-rs/lsd/releases
+    https://github.com/bootandy/dust/releases
+    https://github.com/muesli/duf/releases
+    https://github.com/aristocratos/btop/releases
+    https://github.com/dundee/gdu"
+   
+    #create aliases to links new programs to defaults
+    echo -e 'alias yay="sudo dnf update"
+    alias htop="btop"
+    alias du="dust"
+    alias df="duf"
+    alias cat="bat -pp -P"
+    alias nano="micro"
+    alias ls="lsd"
+    alias ncdu="gdu"' >> .zshrc  
+}
+
+drop_proxy_config_git () {
+    git config --global --unset http.proxy
+    git config --global --unset http.proxyAuthMethod
+    git config --global --unset http.sslVerify 
+}
+
+main () {
+    install_git_zsh
+    config_proxy
+    install_oh_my_zsh
+    install_plugins
+    install_powerlevel
+    fix_zsh_docker
+    config_font
+    change_shell
+    linux_2023
+    drop_proxy_config_git
+}
+
+main "$@"
