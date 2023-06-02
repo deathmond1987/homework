6e816d381eb57e2b8403e69256bbb174340464a9
deathmond1987 <deathmond1987@gmail.com>
2023-06-02 11:05:35 +0300

    Update zsh_home_install.sh

--- zsh_home_install.sh
+++ zsh_home_install.sh
@@ -33,12 +33,13 @@ install_git_zsh () {
 
 config_proxy () {
     if [ -n "$HTTP_PROXY" ]; then
-        #get oh-my-zsh
-        sh -c "$(curl -fsSL -x "$HTTP_PROXY" https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
         #config git with proxy
         git config --global http.proxy http://"$HTTP_PROXY"
         git config --global http.proxyAuthMethod 'basic' 
         git config --global http.sslVerify false
+        #get oh-my-zsh
+        sh -c "$(curl -fsSL -x "$HTTP_PROXY" https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
     fi
 }
 
