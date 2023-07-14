8a2e76903ffd12fadfbb36ab1ab5df95a6f2f3aa
deathmond1987 <deathmond1987@gmail.com>
2023-07-14 11:33:04 +0300

    Update archlinux_on_IMG.sh

--- archlinux_on_IMG.sh
+++ archlinux_on_IMG.sh
@@ -140,7 +140,7 @@ exit_trap () {
         umount "$MOUNT_PATH"/boot || true
         umount "$MOUNT_PATH"/boot/efi || true
         umount "$MOUNT_PATH" || true
-        lvremove /dev/arch/root || true
+        # lvremove /dev/arch/root || true
         losetup -d "$DISK" || true
         echo "trap finished"
     }
