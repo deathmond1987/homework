13b55049176aedb42aad8b25f086d716d6fc04ce
deathmond1987 <deathmond1987@gmail.com>
2023-08-07 13:41:04 +0300

    Update archlinux_on_IMG.sh

--- archlinux_on_IMG.sh
+++ archlinux_on_IMG.sh
@@ -463,23 +463,30 @@ LC_TIME=en_US.UTF-8' > /etc/locale.conf
 
     other_config () {
         # enabling pacman from game
-        echo "
-ILoveCandy" >> /etc/pacman.conf
+        # echo "
+        # ILoveCandy" >> /etc/pacman.conf
         #enabling parallel downloads in pacman
         sed -i '/ParallelDownloads = 5/s/^#//g' /etc/pacman.conf
         #enabling colors in pacman output
         sed -i '/Color/s/^#//g' /etc/pacman.conf
+        
         # changing default mc theme
         echo "export MC_SKIN=gotar" >> /etc/profile
+        
+        # enabling hstr alias
         echo "export HISTFILE=~/.zsh_history" >> /home/kosh/.zshrc
         echo 'alias history="hstr"' >> /home/kosh/.zshrc
 
+        # changing grub config
+        sed -i 's/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=countdown/g' /etc/default/grub
+        sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3"/g' /etc/default/grub
+        
         # downloading tor fork for docker
         mkdir -p /opt/tor
         wget -O /opt/tor/docker-compose.yml https://raw.githubusercontent.com/deathmond1987/docker-tor/main/docker-compose.yml
 
-        # workaround slow mc start: disable subshell
-        echo 'alias mc="mc --nosubshell"' >> /home/kosh/.zshrc
+        # workaround slow mc start. long time to create subshell for mc. we will load mc from bash
+        echo 'alias mc="SHELL=/bin/bash /usr/bin/mc"' >> /home/kosh/.zshrc
 }
 
     main () {
