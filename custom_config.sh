573e0348d22e67d65dab3cc26b2ff7889d731c54
deathmond1987 <deathmond1987@gmail.com>
2023-08-10 09:09:54 +0300

    Update custom_config.sh

--- custom_config.sh
+++ custom_config.sh
@@ -1,6 +1,44 @@
 #!/usr/bin/env bash
 set -xe 
 
+if [ "$WSL_INSTALL" = "true" ]; then
+    success "configuring wsl..."
+    echo "[boot]
+systemd=true
+[user]
+default=kosh
+[automount]
+enabled = true
+options = \"metadata\"
+mountFsTab = true" > "$MOUNT_PATH"/etc/wsl.conf
+    #Under wsl thereis issue in memory cache. We will drop memory caches with systemd unit every 3 minute
+    echo -e "[Unit]
+Description=Periodically drop caches to save memory under WSL.
+Documentation=https://github.com/arkane-systems/wsl-drop-caches
+ConditionVirtualization=wsl
+Requires=drop_cache.timer
+
+[Service]
+Type=oneshot
+ExecStartPre=sync
+ExecStart=echo 3 > /proc/sys/vm/drop_caches" > "$MOUNT_PATH"/etc/systemd/system/drop_cache.service
+
+    echo -e "[Unit]
+Description=Periodically drop caches to save memory under WSL.
+Documentation=https://github.com/arkane-systems/wsl-drop-caches
+ConditionVirtualization=wsl
+PartOf=drop_cache.service
+
+[Timer]
+OnBootSec=3min
+OnUnitActiveSec=3min
+
+[Install]
+WantedBy=timers.target" > "$MOUNT_PATH"/etc/systemd/system/drop_cache.timer
+
+    systemctl enable drop_cache.timer
+    systemctl disable systemd-networkd-wait-online
+fi
 
 # PACMAN CONF
 # enabling pacman from game
@@ -45,7 +83,9 @@ su - kosh -c "wget -qO - https://raw.githubusercontent.com/deathmond1987/homewor
         
 # changing default mc theme
 echo "MC_SKIN=gotar" >> /etc/environment
-        
+# not work in wsl with default user in wsl.conf
+echo "MC_SKIN=gotar" >> /home/kosh/.zshcr
+
 # enabling hstr alias
 echo "export HISTFILE=~/.zsh_history" >> /home/kosh/.zshrc
 echo 'alias history="hstr"' >> /home/kosh/.zshrc
@@ -65,3 +105,8 @@ wget -O /opt/tor/docker-compose.yml https://raw.githubusercontent.com/deathmond1
 # enabling units
 systemctl enable docker.service
 systemctl enable sshd.service
+
+# changing sudo rules to disable executing sudo without password
+sed -i 's/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers
+# allow wheel group using sudo with password
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/g' /etc/sudoers
