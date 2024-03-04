f21cdab5e0de04b4d877932ed326101b4fb49eee
deathmond1987 <deathmond1987@gmail.com>
2024-03-04 18:00:51 +0300

    Update archlinux_on_IMG.sh
    duplicate code removed

--- archlinux_on_IMG.sh
+++ archlinux_on_IMG.sh
@@ -772,28 +772,8 @@ qemu_install () {
             OVMF_PATH=/usr/share/edk2/x64/OVMF_CODE.fd
         else
             echo "Unknown OS"
+            exit 1
         fi    
-        qemu-img resize ./vhd.img 12G
-        qemu-img convert -p -f raw -O vhdx ./vhd.img ./vhd.vhdx
-        success "VHDX image for HYPER-V created."
-        warn "$(ls -la | grep vhd.vhdx)"
-        qemu-img convert -p -f raw -O vmdk ./vhd.img ./vhd.vmdk
-        success "VMDK image for VMWARE created."
-        warn "$(ls -la | grep vhd.vmdk)"
-        readlink -p "Execute image in qemu? (y/n)" $answer
-        case $answer in 
-            y) qemu-system-x86_64 \
-            -enable-kvm \
-            -smp cores=4 \
-            -m 2G \
-            -drive if=pflash,format=raw,readonly=on,file="$OVMF_PATH" \
-            -device nvme,drive=drive0,serial=badbeef \
-            -drive if=none,id=drive0,file=./vhd.img
-            ;;
-            n) true
-            ;;
-        esac
-    fi
 }
 
 export_image_hyperv () {
