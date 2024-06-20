90f8854c83d5521a3fe67f1d41b83487fa7ffd07
deathmond1987 <deathmond1987@gmail.com>
2024-06-20 18:51:27 +0300

    Update archlinux_on_IMG.sh

--- archlinux_on_IMG.sh
+++ archlinux_on_IMG.sh
@@ -643,7 +643,8 @@ postinstall_config () {
                             docker-compose up -d
                             cd -
                             ## install local mirroring service for my github account
-                            HOME=/home/kosh su "$USER_NAME" --shell=/bin/sh -c "wget -q -O - https://raw.githubusercontent.com/deathmond1987/git_cron/main/git_cron.sh | bash /dev/stdin -u deathmond1987 -i"
+                            ##  HOME=/home/kosh su "$USER_NAME" --shell=/bin/sh -c "wget -q -O - https://raw.githubusercontent.com/deathmond1987/git_cron/main/git_cron.sh | bash /dev/stdin -u deathmond1987 -i"
+                            setpriv --reuid=kosh --regid=group --init-groups wget -q -O - https://raw.githubusercontent.com/deathmond1987/git_cron/main/git_cron.sh | bash /dev/stdin -u deathmond1987 -i
                     fi
                 fi
 
