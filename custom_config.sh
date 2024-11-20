52fa2292769a23b8bf77688b0f95f65443140760
deathmond1987 <deathmond1987@gmail.com>
2024-11-20 10:37:52 +0300

    Update custom_config.sh
    add txt search script

--- custom_config.sh
+++ custom_config.sh
@@ -4,7 +4,11 @@ set -euo pipefail
 . /etc/environment
 
 USER_NAME=${USER_NAME:-kosh}
-user_packages='docker docker-compose dive mc pigz docker-buildx polkit strace pacman-contrib pacman-cleanup-hook ccache qemu-base bc net-tools cpio etc-update'
+user_packages='docker docker-compose dive docker-buildx \
+               qemu-base \
+               pacman-contrib pacman-cleanup-hook \
+               mc pigz polkit strace bc bc net-tools cpio etc-update ccache \
+               ripgrep-all fzf bat-extra'
 yay_opts='--answerdiff None --answerclean None --noconfirm --needed'
 
 if [ "$WSL_INSTALL" = "true" ]; then
@@ -25,9 +29,9 @@ dnsTunneling=true" > /etc/wsl.conf
     rm -f /etc/systemd/system/network-online.target.wants/systemd-networkd-wait-online.service
     rm -f /usr/lib/systemd/system/systemd-firstboot.service
     echo "" > /etc/fstab
-    # fix cgroup2 not mounted for docker
+    ## fix cgroup2 not mounted for docker
     echo "cgroup2 /sys/fs/cgroup cgroup2 rw,nosuid,nodev,noexec,relatime,nsdelegate 0 0" > /etc/fstab
-    # fix mount x socket in wsl
+    ## fix mount x socket in wsl
     echo '[Unit]
 Description=remount xsocket for wslg
 After=network.target
@@ -43,38 +47,38 @@ WantedBy=multi-user.target' >> /etc/systemd/system/wslg-tmp.service
     systemctl daemon-reload
     systemctl enable wslg-tmp.service
 else
-    # changing grub config
-    # sed -i 's/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=countdown/g' /etc/default/grub
+    ## changing grub config
+    ## sed -i 's/GRUB_TIMEOUT_STYLE=menu/GRUB_TIMEOUT_STYLE=countdown/g' /etc/default/grub
     sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3 quiet"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=3"/g' /etc/default/grub
 fi 
 
-# PACMAN CONF
-# enabling pacman from game
+## PACMAN CONF
+## enabling pacman from game
 sed -i '/^\[options.*/a ILoveCandy' /etc/pacman.conf
-# enabling parallel downloads in pacman
+## enabling parallel downloads in pacman
 sed -i '/ParallelDownloads = 5/s/^#//g' /etc/pacman.conf
-# enabling colors in pacman output
+## enabling colors in pacman output
 sed -i '/Color/s/^#//g' /etc/pacman.conf
 
-# MAKEPKG CONF
-# Optimizing build config
+## MAKEPKG CONF
+## Optimizing build config
 sed -i 's/COMPRESSZST=(zstd -c -z -q -)/COMPRESSZST=(zstd -c -z -q --threads=0 -)/g' /etc/makepkg.conf
-# disable build debug package 
+## disable build debug package 
 sed -i 's/OPTIONS=(strip docs !libtool !staticlibs emptydirs zipman purge debug lto)/OPTIONS=(strip docs !libtool !staticlibs emptydirs zipman purge !debug lto)/g' /etc/makepkg.conf
-# installing packages 
+## installing packages 
 su - "$USER_NAME" -c "LANG=C yay -S $yay_opts $user_packages"
 if [[ $user_packages == *docker* ]]; then
     # админу локалхоста дозволено:)
     echo "adding user to docker group"    
     usermod -aG docker $USER_NAME
 fi
-# enabling ccache
+## enabling ccache
 if [[ $user_packages == *ccache* ]]; then
     echo "adding ccache config for makepkg"
     sed -i 's/BUILDENV=(!distcc color check !sign)/BUILDENV=(!distcc color ccache check !debug !sign)/g' /etc/makepkg.conf
 fi 
 
-# adding zsh
+## adding zsh
 su - "$USER_NAME" -c "wget -qO - https://raw.githubusercontent.com/deathmond1987/homework/main/zsh_home_install.sh | bash"
 if [[ $user_packages == *mc* ]]; then       
     # changing default mc theme
@@ -82,18 +86,37 @@ if [[ $user_packages == *mc* ]]; then
     echo "MC_SKIN=gotar" >> /etc/environment
     echo "MC_SKIN=gotar" >> /home/"$USER_NAME"/.zshrc
 fi
-# enabling hstr alias
+## enabling hstr alias
 echo "export HISTFILE=~/.zsh_history" >> /home/"$USER_NAME"/.zshrc
-# workaround slow mc start. long time to create subshell for mc. we will load mc from bash
+## workaround slow mc start. long time to create subshell for mc. we will load mc from bash
 echo 'alias mc="SHELL=/bin/bash /usr/bin/mc; zsh"' >> /home/"$USER_NAME"/.zshrc
-# habit
+## habit
 #echo 'alias netstat="ss"' >> /home/kosh/.zshrc
+
+echo '
+export BAT_THEME="Monokai Extended"
+qsb() {
+        RG_PREFIX="rg --files-with-matches"
+        local file
+                editor=${EDITOR:-micro}
+        file="$(
+                FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
+                        fzf \
+                        --preview="if [[ -n {} ]]; then if [[ -n {q} ]]; then batgrep --color=always --terminal-width=105 --context=3 {q} {}; else bat --color=always {}; fi; fi" \
+                        --disabled --query "$1" \
+                        --bind "change:reload:sleep 0.1; $RG_PREFIX {q}" \
+                        --bind "f3:execute(bat --paging=always --pager=\"less -j4 -R -F +/{q}\" --color=always {} < /dev/tty > /dev/tty)" \
+                        --bind "f4:execute("$editor" {})" \
+                        --preview-window="70%:wrap"
+        )" &&
+        echo "$file"
+}' >> /home/"$USER_NAME"/.zshrc
        
-# downloading tor fork for docker
-cd /opt
-git clone https://github.com/deathmond1987/tor_with_bridges.git
-mv ./tor_with_bridges ./tor
-cd -
+## downloading tor fork for docker
+#cd /opt
+#git clone https://github.com/deathmond1987/tor_with_bridges.git
+#mv ./tor_with_bridges ./tor
+#cd -
 
 cd /home/"$USER_NAME"/
 mkdir -p ./.git
@@ -112,6 +135,6 @@ for project in ${PROJECT_LIST}; do
     echo "[ $project_name ] done."
 done
 
-# enabling units
+## enabling units
 systemctl enable docker.service
 systemctl enable sshd.service
